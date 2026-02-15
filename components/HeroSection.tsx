b8c4c00e12101bbc845a472ad5d8654eb6a43612
ZakariaElkhadir <zelkhadir5@gmail.com>
2026-02-15 16:05:42 +0000

    fix: Add minimum height to HeroSection for better layout consistency

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -3,7 +3,7 @@ import ScrollAnimation from "./ScrollAnimation";
 
 const HeroSection = () => {
   return (
-    <section className="w-full bg-light-bg px-5 pt-8 sm:pt-0 overflow-hidden">
+    <section className="w-full bg-light-bg px-5 pt-8 sm:pt-0 overflow-hidden min-h-[calc(100vh-3.25rem)]">
       <div className="max-w-7xl mx-auto flex flex-col-reverse lg:flex-row items-center gap-10 lg:gap-12">
         <div className="flex-1 text-center lg:text-left">
           <ScrollAnimation animation="fade-up" delay={0.2}>
@@ -27,7 +27,13 @@ const HeroSection = () => {
         </div>
         <div className="flex-1 flex justify-center lg:justify-end lg:-mr-32 w-full">
           <ScrollAnimation animation="fade-in" delay={0.8} className="w-full flex justify-center lg:justify-end">
-            <Image src="/images/heroimage1.png" alt="Hero Image" width={1000} height={600} className="w-full max-w-lg lg:max-w-none" />
+            <Image
+              src="/images/heroimage1.png"
+              alt="Hero Image"
+              width={1000}
+              height={600}
+              className="w-full max-w-lg lg:max-w-none max-h-[calc(100vh-3.25rem)] object-contain"
+            />
           </ScrollAnimation>
         </div>
       </div>
